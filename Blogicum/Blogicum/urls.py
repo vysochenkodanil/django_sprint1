340deee7268509c7ef46317482cef0d7e01068a3
Данил Высоченко <vysochenkodanil@gmail.com>
2024-12-08 13:52:42 +0300

    исправленный

--- Blogicum/Blogicum/urls.py
+++ Blogicum/Blogicum/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')), 
-    path('pages/', include('pages.urls', namespace='pages')), 
+    path('', include('blog.urls', namespace='blog')),
+    path('pages/', include('pages.urls', namespace='pages')),
 ]
